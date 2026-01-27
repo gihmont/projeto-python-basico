13d0ea840b406213672eadff5102326a61531fda
Giovanna Monteiro Lima <giovannamonteiro1303@gmail.com>
2026-01-27 07:34:32 -0300

    inserção de loop
    Testando e explorando as estruturas de loop, sendo utilizado então a estrutura while.

---
+++ Projeto/gerenciador.py
@@ -0,0 +1,14 @@
+while True:
+    print("\nMenu do Gerenciador de Tarefas")
+    print("1. Adicionar Tarefa")
+    print("2. Ver tarefas")
+    print("3. Atualizar tarefa")
+    print("4. Completar tarefa")
+    print("5. Deletar tarefas completadas")
+    print("6. Sair")
+
+    escolha = input("Digite a sua escolha: ")
+
+    if escolha == "6":
+        break
+print("Programa finalizado.")
