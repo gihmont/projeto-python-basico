205ab2c1f4254a83c1e4de51af1c645652365ae1
Giovanna Monteiro Lima <giovannamonteiro1303@gmail.com>
2026-01-28 20:41:17 -0300

    Deletar tarefas completadas
    Inserção da função de deletar as tarefas sinalizadas como concluídas pelo usuário.

--- Projeto/gerenciador.py
+++ Projeto/gerenciador.py
@@ -31,6 +31,14 @@ def completar_tarefa(tarefas, indice_tarefa):
     print(f"Tarefa {indice_tarefa} marcada como completada")
     return
 
+#Função para deletar as tarefas concluídas
+def deletar_tarefas_completadas(tarefas):
+    for tarefa in tarefas:
+        if tarefa["completada"]:
+            tarefas.remove(tarefa)
+    print("Tarefas completadas foram deletadas")
+    return
+
 tarefas = []
 
 #Menu do gerenciador
@@ -59,7 +67,9 @@ while True:
         ver_tarefas(tarefas)
         indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
         completar_tarefa(tarefas, indice_tarefa)
-
+    elif escolha == "5":
+        deletar_tarefas_completadas(tarefas)
+        ver_tarefas(tarefas)   
     elif escolha == "6":
         break
 print("Programa finalizado.")
