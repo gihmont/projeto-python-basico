66b589df10a42e1b921079bec96b0db1781c63a5
Giovanna Monteiro Lima <giovannamonteiro1303@gmail.com>
2026-01-28 19:29:30 -0300

    Adicionar tarefa
    Inserção de uma função no qual adiciona a tarefa desejada do usuário.

--- Projeto/gerenciador.py
+++ Projeto/gerenciador.py
@@ -1,3 +1,13 @@
+#Função para adicionar uma tarefa
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefa = {"nome": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso.")
+    return
+
+tarefas = []
+
+#Menu do gerenciador
 while True:
     print("\nMenu do Gerenciador de Tarefas")
     print("1. Adicionar Tarefa")
@@ -9,6 +19,9 @@ while True:
 
     escolha = input("Digite a sua escolha: ")
 
-    if escolha == "6":
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "6":
         break
 print("Programa finalizado.")
