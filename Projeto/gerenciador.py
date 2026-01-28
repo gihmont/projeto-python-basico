1d7fd2e68b0fd481099a156060e3c83983137fc6
Giovanna Monteiro Lima <giovannamonteiro1303@gmail.com>
2026-01-28 19:52:00 -0300

    Ver tarefa
    Inserção da função de visualizar a(as) tarefa(as) adicionada(as) pelo usuário.

--- Projeto/gerenciador.py
+++ Projeto/gerenciador.py
@@ -5,6 +5,15 @@ def adicionar_tarefa(tarefas, nome_tarefa):
     print(f"Tarefa {nome_tarefa} foi adicionada com sucesso.")
     return
 
+#Função para visualizar uma tarefa
+def ver_tarefas(tarefas):
+    print("\nLista de tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status = "✓" if tarefa["completada"] else " "
+        nome_tarefa = tarefa["nome"]
+        print(f"{indice}. [{status}] {nome_tarefa}")
+    return
+
 tarefas = []
 
 #Menu do gerenciador
@@ -22,6 +31,8 @@ while True:
     if escolha == "1":
         nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
         adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "2":
+        ver_tarefas(tarefas)
     elif escolha == "6":
         break
 print("Programa finalizado.")
